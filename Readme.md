8659e6dc778353e56e0b0505d6028c198d50de15
Aaron James Young <aaron@ajy.co>
2014-09-08 22:37:39 -0700

    initial commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Spammer Killer
+
+A lightweight WordPress plugin to kill comment spam.
+
+## How to use it
+
+Insert the spammer-killer folder (or just the spammer-killer.php file) into your wp-content/plugins directory. Log in to WordPress, go to your Plugins page, and activate the plugin.
+
+Boom! No more spam comments!
+
+## How it works
+
+This is your basic antispam honeytrap. You see, spambots tend to fill out every field in a web form. So, we just put a little text field in there, hide it with CSS, and check it before processing the comment. If it contains a value, we presume a spambot filled out the form and we stop the comment right there.
+
+## That's it?
+
+Yep. And I'd say it's 99% effective. Remember that it won't stop real people writing spammy comments on your blog, but it's pretty good at getting the bulk of the spam.
+
+Enjoy!
---
+++ ajy-spammer-killer.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* Plugin Name: Spammer Killer
+* Plugin URI: http://github.com/aaronjamesyoung/ajy-spammer-killer
+* Description: Fights WordPress comment spam with a honeypot trap.
+* Version: 100000
+* Author: Aaron James Young
+* Author URI: http://ajy.co
+* License: GPL3
+*/
+
+// Add the honeypot to the comment form
+add_action( 'comment_form_logged_in_after', 'ajy_spam_field' );
+add_action( 'comment_form_after_fields', 'ajy_spam_field' );
+function ajy_spam_field()
+{
+    ?>
+    <p class="ajy-extra-field">
+        <label for="ajy_extra_field">Leave this field empty to prove you are human</label>
+        <input type="text" name="ajy_extra_field" id="ajy_extra_field" />
+    </p>
+    <style>
+      .ajy-extra-field { display: none !important; visibility: hidden !important; height: 0 !important; width: 0 !important; overflow: hidden !important; }
+    </style>
+    <?php
+}
+
+// Kill the submission if the bot filled out the honeypot
+function ajy_new_comment($commentdata) {
+  if($_POST['ajy_extra_field'] != "") {
+    die('You are spam');
+  }
+  return $commentdata;
+}
+add_filter('preprocess_comment', 'ajy_new_comment');
